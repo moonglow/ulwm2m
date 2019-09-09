80c40c4b909a3e7f1866bf1b945cf65b2cf60929
moonglow <fxdteam@gmail.com>
2019-09-09 15:15:37 +0300

    +add observe handler

--- example/object_3303.c
+++ example/object_3303.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include "object_3303.h"
 
 /* based on leshan server predefined object 3303  */
@@ -12,16 +13,23 @@
 
 static const uint16_t id_0_list[] = 
 {
-        ID_R_SENSOR_VALUE, ID_R_MIN_MEAS_VALUE,
-        ID_R_MAX_MEAS_VALUE, ID_R_MIN_RANGE_VALUE,
-        ID_R_MAX_RANGE_VALUE, ID_R_SENSOR_UNITS
+  ID_R_SENSOR_VALUE, ID_R_MIN_MEAS_VALUE,
+  ID_R_MAX_MEAS_VALUE, ID_R_MIN_RANGE_VALUE,
+  ID_R_MAX_RANGE_VALUE, ID_R_SENSOR_UNITS
 };
+
+static const uint16_t id_0_observe_list[] =
+{
+  ID_R_SENSOR_VALUE
+};
+
+struct t_lwm2m_observe_context observe_state =  { .timeout = 2000 };
 
 static float sensor_value, min_value = 100.0f, max_value = 0.0f;
 static const float sensor_min_range = 0.0f, sensor_max_range = 100.0f;
 static const char sz_units[] = "Cel";
 
-int object_3303_read(struct t_lwm2m_item *parg )
+int object_3303_read(struct t_lwm2m_data *parg )
 {
   switch( parg->id )
   {
@@ -66,13 +74,18 @@ int object_3303_read(struct t_lwm2m_item *parg )
       parg->data = (void*)id_0_list;
       parg->size = sizeof( id_0_list );
       break;
+    case LWM2M_GET_OBSERVE_LIST:
+      parg->data_type = LWM2M_ITEM_OBSERVE_ID_LIST;
+      parg->data = (void*)id_0_observe_list;
+      parg->size = sizeof( id_0_observe_list );
+      break;
     default:
       return -1;
   }
   return 1;
 }
 
-int object_3303_write(struct t_lwm2m_item *parg )
+int object_3303_write(struct t_lwm2m_data *parg )
 {
   switch( parg->id )
   {
@@ -81,7 +94,7 @@ int object_3303_write(struct t_lwm2m_item *parg )
   }
 }
 
-int object_3303_exec(struct t_lwm2m_item *parg )
+int object_3303_exec(struct t_lwm2m_data *parg )
 {
   switch( parg->id )
   {
@@ -94,4 +107,10 @@ int object_3303_exec(struct t_lwm2m_item *parg )
   }
 
   return 1;
+}
+
+int object_3303_observe(struct t_lwm2m_data *parg, int control, uint32_t timestamp )
+{
+  /* call default observe handler */
+  return lwm2m_process_observe_control( parg, &observe_state, control, timestamp );
 }
--- example/object_3303.h
+++ example/object_3303.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <lwm2m.h>
 
-int object_3303_read(struct t_lwm2m_item *parg );
-int object_3303_write(struct t_lwm2m_item *parg );
-int object_3303_exec(struct t_lwm2m_item *parg );
+int object_3303_read(struct t_lwm2m_data *parg );
+int object_3303_write(struct t_lwm2m_data *parg );
+int object_3303_exec(struct t_lwm2m_data *parg );
+int object_3303_observe(struct t_lwm2m_data *parg, int control, uint32_t timestamp );
