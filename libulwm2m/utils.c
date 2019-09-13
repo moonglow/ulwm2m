c206bad553b9b0b8c4ebe6ed9a97b595f9f382e4
moonglow <fxdteam@gmail.com>
2019-09-13 15:26:55 +0300

    +add string to int utils

---
+++ libulwm2m/utils.c
@@ -0,0 +1,33 @@
+#include "utils.h"
+
+int _strntoi( const char *s, int n )
+{
+  int v = 0;
+
+  while( n-- )
+  {
+    char ch = *s++;
+    if( ch < '0' || ch > '9' )
+      break;
+    v *= 10;
+    v += ch - '0';
+  }
+
+  return v;
+}
+
+int _strtoi( const char *s )
+{
+  int v = 0;
+  char ch;
+
+  while( ( ch = *s++ ) )
+  {
+    if( ch < '0' || ch > '9' )
+      break;
+    v *= 10;
+    v += ch - '0';
+  }
+
+  return v;
+}
---
+++ libulwm2m/utils.h
@@ -0,0 +1,5 @@
+#pragma once
+
+/* convert string to int for specified nums of chars */
+int _strntoi( const char *s, int n );
+int _strtoi( const char *s );
